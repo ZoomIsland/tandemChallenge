bf9ae8d5802d7f0814883b01254606f487ba21b0
ZoomIsland <zacherymilan@gmail.com>
2020-10-30 15:42:26 -0600

    Unstyled, but meets the brief

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,22 @@ let currentDivSelect = ".q" + currentQuestion.toString();
 let answers = [];
 
 $(".startBtn").on("click", () => {
+  $(".startBtn").addClass("hidden")
   $(currentDivSelect).removeClass("hidden");
 })
 
 $(".answerBtn").on("click", (event) => {
-  answers.push(event.target.value)
-  console.log(answers)
+  let answer = event.target.value
   const target = $( event.target )
+  const responseDiv = target.parent().parent().children(".correctOrWrong")
+  if (answer === "false") {
+    target.addClass("wrong")
+    responseDiv.children(".wrongAnswer").removeClass("hidden")
+  } else {
+    answers.push(answer)
+    target.addClass("correct")
+    responseDiv.children(".correctAnswer").removeClass("hidden")
+  }
   target.prop("disabled", true)
   target.siblings().prop("disabled", true)
   target.parent().parent().children('.nextBtn').removeClass("hidden")
@@ -21,6 +30,11 @@ $(".nextBtn").on("click", () => {
   currentDivSelect = ".q" + currentQuestion.toString();
   if (currentQuestion < 10) {
     $(currentDivSelect).removeClass("hidden");
+  } else {
+    const resultsDiv = $(".results")
+    const result = (answers.length / 10) * 100;
+    resultsDiv.children('.resultText').text(`You answered ${result}% correct!`)
+    resultsDiv.removeClass("hidden");
   }
 })
 
